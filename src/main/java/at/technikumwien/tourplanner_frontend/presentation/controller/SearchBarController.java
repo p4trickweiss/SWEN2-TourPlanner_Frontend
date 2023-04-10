ae9ed6ae14ef70363f29917a3f12ea4b9556ab42
Patrick Weiss <p4trick.weiss@gmail.com>
2023-04-10 11:06:36 +0200

    extracted searchbar from mainWindow.fxml

---
+++ src/main/java/at/technikumwien/tourplanner_frontend/presentation/controller/SearchBarController.java
@@ -0,0 +1,13 @@
+package at.technikumwien.tourplanner_frontend.presentation.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SearchBarController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
