b5fd017cbb9bba83dfb69085cd4237941f1192f2
Patrick Weiss <p4trick.weiss@gmail.com>
2023-05-19 13:41:59 +0200

    General Tab & Misc Tab Update

--- src/main/java/at/technikumwien/tourplanner_frontend/presentation/controller/TourInfoController.java
+++ src/main/java/at/technikumwien/tourplanner_frontend/presentation/controller/TourInfoController.java
@@ -2,16 +2,18 @@ package at.technikumwien.tourplanner_frontend.presentation.controller;
 
 import at.technikumwien.tourplanner_frontend.model.Tour;
 import at.technikumwien.tourplanner_frontend.presentation.viewmodel.TourInfoViewModel;
-import at.technikumwien.tourplanner_frontend.presentation.viewmodel.TourListViewModel;
 import at.technikumwien.tourplanner_frontend.presentation.viewmodel.ViewModelFactory;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TabPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.text.Text;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -24,43 +26,44 @@ public class TourInfoController implements Initializable {
     private static Logger logger = LogManager.getLogger(TourInfoController.class);
 
     @FXML
+    public ColumnConstraints textWidth;
+    @FXML
     public ImageView imageView;
     @FXML
-    public TableColumn<Tour, String> tour_description;
+    public Text tour_description;
     @FXML
-    public TableColumn<Tour, String> tour_distance;
+    public Text tour_distance;
     @FXML
-    public TableColumn<Tour, String> tour_from;
+    public Text tour_from;
     @FXML
-    public TableColumn<Tour, String> tour_to;
+    public Text tour_to;
     @FXML
-    public TableColumn<Tour, String> transport_type;
+    public Text transport_type;
     @FXML
-    public TableView<Tour> tableViewTourDetails;
-
-    public TableColumn<Tour, String> time_stamp;
+    public Text time_stamp;
     @FXML
-    public TableColumn<Tour, String> comment;
+    public Text comment;
     @FXML
-    public TableColumn<Tour, String> difficulty;
+    public Text difficulty;
     @FXML
-    public TableColumn<Tour, String> total_time;
+    public Text total_time;
     @FXML
-    public TableColumn<Tour, String> rating;
+    public Text rating;
     @FXML
-    public TableView<Tour> tableViewMisc;
     private final TourInfoViewModel tourInfoViewModel = ViewModelFactory.INSTANCE.getTourInfoViewModel();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         logger.info("TourInfoController init");
 
+        //tour_description.wrappingWidthProperty().bind(textWidth.percentWidthProperty());
+
+        //General
         tour_description.textProperty().bindBidirectional(tourInfoViewModel.tour_descriptionProperty());
         tour_distance.textProperty().bindBidirectional(tourInfoViewModel.tour_distanceProperty());
         tour_from.textProperty().bindBidirectional(tourInfoViewModel.tour_fromProperty());
         tour_to.textProperty().bindBidirectional(tourInfoViewModel.tour_toProperty());
         transport_type.textProperty().bindBidirectional(tourInfoViewModel.transport_typeProperty());
-        //Tabelle befüllen
 
         // Image anzeigen lassen
         imageView.imageProperty().bind(Bindings.createObjectBinding(() -> {
